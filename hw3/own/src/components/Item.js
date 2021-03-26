d3be59bb8bb5dd9a557c8500d21700b3ac5d32af
johnsonhung906 <khhung906@gmail.com>
2021-03-26 22:47:40 +0800

    hw3

--- hw3/own/src/components/Item.js
+++ hw3/own/src/components/Item.js
@@ -5,8 +5,8 @@ class Todo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isDone: 0,
-            pStyle: {}
+            pStyle: {},
+            tStyle: {}
         }
     }
     
@@ -19,21 +19,23 @@ class Todo extends React.Component {
         const{Completed, id} = this.props;
         if(Completed.includes(id)){
             this.setState({pStyle: {opacity: 0.5,
-                textDecorationLine: ' line-through'}})
+                                    textDecorationLine: ' line-through'}});
+            this.setState({tStyle:{background: '#26ca299b'}});
         }
         else{
-            this.setState({pStyle: {}});
+            this.setState({pStyle:{}});
+            this.setState({tStyle:{}});
         }
     }
+    
     delete = () => {
         const { text, deleteTodo } = this.props;
         deleteTodo(text);
     };
     
     done = () =>{
-        const { text, Done } = this.props;
-        this.setState({isDone: -this.state.isDone+1})
-        if(this.state.isDone){
+        const { text, Done, Completed } = this.props;
+        if(Completed.includes(text)){
             Done(0, text);
         }
         else{
@@ -47,7 +49,7 @@ class Todo extends React.Component {
         <li className="todo-app__item">
             <div className="todo-app__checkbox">
                 <input type="checkbox" id={id} onClick={this.done}/>
-                <label htmlFor={id}/>
+                <label htmlFor={id} style={this.state.tStyle}/>
             </div>
             <h1 className="todo-app__item-detail" style={this.state.pStyle}>{text}</h1>
             <img src="./img/x.png" className="todo-app__item-x" onClick={this.delete}/>
