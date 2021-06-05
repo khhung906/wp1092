2053dcaea68b397e0a84975dfabbfa41fdf221ef
johnsonhung906 <khhung906@gmail.com>
2021-06-05 12:53:59 +0800

    hw7

---
+++ hw7/own/backend/mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// i use mongodb://localhost:27017/cardmongo for MONGO_URL
+
+function connectMongo() {
+  mongoose.connect('mongodb://localhost:27017/testmongo', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function () {
+    console.log('Mongo database connected!');
+  });
+}
+
+const mongo = {
+  connect: connectMongo,
+};
+
+module.exports = mongo;
