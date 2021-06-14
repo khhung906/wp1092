58c87a7f32fba26b4b4abfd21d66e62123c33062
Toby Chen <you@example.com>
2021-06-14 22:02:13 +0800

    [TA] Sorry, I forget that the deadline has been extended...

--- hw7/review/1/frontend/src/hooks/useChat.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";  
-const useChat = () => {
-  const [status, setStatus] = useState({}); // { type, msg }
-  const sendMessage = (payload) => {
-    console.log(payload);
-  }; // { key, msg }
-  return { status, sendMessage };
-};
-export default useChat;
